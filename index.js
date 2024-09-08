61bdf7374b2508aa6e9226f7944a0b141de2947d
MSİ <136392359+sadecemsi@users.noreply.github.com>
2024-09-08 18:38:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const croxydb = require('croxydb');
+
+client.commands = new Collection();
+const commands = [];
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+client.once('ready', async () => {
+    console.log('Bot hazır!');
+
+    try {
+        console.log('Slash komutları yükleniyor...');
+
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: commands },
+        );
+
+        console.log('Slash komutları başarıyla yüklendi!');
+    } catch (error) {
+        console.error('Slash komutları yüklenirken bir hata oluştu:', error);
+    }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction, client, croxydb);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Komut yürütülürken bir hata oluştu!', ephemeral: true });
+    }
+});
+
+client.login(token);
